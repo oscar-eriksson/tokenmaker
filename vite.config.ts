79b6092b3b5cb51aee9c45b0b3014c93917abbe0
oscar-eriksson <oscar_eriksson_96@hotmail.com>
2026-03-19 19:38:52 +0100

    fallback version name

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,13 @@ export default defineConfig({
   plugins: [svelte()],
   define: {
     // @ts-ignore
-    __APP_VERSION__: JSON.stringify(process.env.npm_package_version || '1.1.0'),
-    __BUILD_DATE__: JSON.stringify(new Date().toLocaleString('en-GB', { 
-        day: '2-digit', 
-        month: 'short', 
-        year: 'numeric', 
-        hour: '2-digit', 
-        minute: '2-digit' 
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version || 'dev'),
+    __BUILD_DATE__: JSON.stringify(new Date().toLocaleString('en-GB', {
+      day: '2-digit',
+      month: 'short',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
     }))
   }
 })
